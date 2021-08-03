d8f21408fab74d62f56683b7450dd5af2bb3d62a
yunji <zion830@naver.com>
2021-08-04 06:32:45 +0900

    bottom navigation에 menu 추가

--- app/src/main/java/android/milestone/MainActivity.kt
+++ app/src/main/java/android/milestone/MainActivity.kt
@@ -4,6 +4,7 @@ import android.milestone.databinding.ActivityMainBinding
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -16,10 +17,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
         binding.navigation.setupWithNavController(navHostFragment.navController)
+        binding.navigation.menu.findItem(R.id.menu_empty).isEnabled = false
 
         navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
             // binding.navigation.isVisible = destination.id !in listOf() // 추후 화면 추가됐을 때 사용
