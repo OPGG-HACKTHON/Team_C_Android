13ab943c19f97703ce511856037e30558658c801
yunji <zion830@naver.com>
2021-08-04 05:24:27 +0900

    navigation 설정

--- app/src/main/java/android/milestone/MainActivity.kt
+++ app/src/main/java/android/milestone/MainActivity.kt
@@ -1,11 +1,29 @@
 package android.milestone
 
+import android.milestone.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        initViews()
+    }
+
+    private fun initViews() {
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        binding.navigation.setupWithNavController(navHostFragment.navController)
+
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+            // binding.navigation.isVisible = destination.id !in listOf() // 추후 화면 추가됐을 때 사용
+        }
     }
 }
