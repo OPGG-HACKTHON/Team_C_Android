fce65ac66fd43ee86ec0c4e9c6629b0f6f01e0f6
yunji <zion830@naver.com>
2021-08-04 06:39:26 +0900

    menu별로 화면 연결

--- app/src/main/java/android/milestone/MainActivity.kt
+++ app/src/main/java/android/milestone/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.forEach
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
@@ -17,16 +18,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
-        val binding =
-            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
         binding.navigation.setupWithNavController(navHostFragment.navController)
         binding.navigation.menu.findItem(R.id.menu_empty).isEnabled = false
 
         navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
             // binding.navigation.isVisible = destination.id !in listOf() // 추후 화면 추가됐을 때 사용
+        }
+
+        binding.fab.setOnClickListener {
+            navHostFragment.findNavController().navigate(R.id.home)
         }
     }
 }
