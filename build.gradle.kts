c6ae243dbc29760afdb22ba0be67073fdc76b35a
Mino <k3000js@naver.com>
2021-08-08 22:33:55 +0900

    상단 아이콘 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     }
     dependencies {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
-        classpath("com.android.tools.build:gradle:4.2.2")
+        classpath("com.android.tools.build:gradle:7.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
 
         // NOTE: Do not place your application dependencies here; they belong
