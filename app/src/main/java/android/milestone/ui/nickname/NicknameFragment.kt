780bdc947c0609fae253d9acdcf5c969e63de40d
Mino <k3000js@naver.com>
2021-08-12 16:58:40 +0900

    Nickname 페이지 추가

---
+++ app/src/main/java/android/milestone/ui/nickname/NicknameFragment.kt
@@ -0,0 +1,12 @@
+package android.milestone.ui.nickname
+
+import android.milestone.R
+import android.milestone.base.BaseFragment
+import android.milestone.databinding.FragmentNicknameBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NicknameFragment : BaseFragment<FragmentNicknameBinding>(R.layout.fragment_nickname) {
+    override fun initViews() {
+    }
+}
