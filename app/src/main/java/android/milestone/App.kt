c63e6ed6dffc61c12e2c0115d1944308ea55f077
Mino <k3000js@naver.com>
2021-08-16 22:57:25 +0900

    app 코드 정렬

--- app/src/main/java/android/milestone/App.kt
+++ app/src/main/java/android/milestone/App.kt
@@ -5,7 +5,7 @@ import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App :Application(){
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, BuildConfig.kakao_app_key)
