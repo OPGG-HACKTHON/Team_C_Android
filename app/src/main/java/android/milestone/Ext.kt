8f92015d2e9084c61a145e56c1d4a405c6c494c6
Mino <k3000js@naver.com>
2021-08-16 22:48:03 +0900

    first().isWhitespace()로 변경 및 괄호 통일

--- app/src/main/java/android/milestone/Ext.kt
+++ app/src/main/java/android/milestone/Ext.kt
@@ -6,5 +6,7 @@ fun CharSequence.validation(): Boolean {
     val ps = Pattern.compile("^[a-zA-Z0-9가-힁ㄱ-ㅎ\\s]+$")
     return if (this.length < 2) {
         false
-    } else this.first() != ' ' && ps.matcher(this).matches()
+    } else {
+        !this.first().isWhitespace() && ps.matcher(this).matches()
+    }
 }
