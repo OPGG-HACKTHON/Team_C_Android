8f490b3392b2d4353f872a3d17f88ded51eaf401
Mino <k3000js@naver.com>
2021-08-16 22:49:29 +0900

    patternString으로 변수명 변경

--- app/src/main/java/android/milestone/Ext.kt
+++ app/src/main/java/android/milestone/Ext.kt
@@ -3,10 +3,10 @@ package android.milestone
 import java.util.regex.Pattern
 
 fun CharSequence.validation(): Boolean {
-    val ps = Pattern.compile("^[a-zA-Z0-9가-힁ㄱ-ㅎ\\s]+$")
+    val patternString = Pattern.compile("^[a-zA-Z0-9가-힁ㄱ-ㅎ\\s]+$")
     return if (this.length < 2) {
         false
     } else {
-        !this.first().isWhitespace() && ps.matcher(this).matches()
+        !this.first().isWhitespace() && patternString.matcher(this).matches()
     }
 }
