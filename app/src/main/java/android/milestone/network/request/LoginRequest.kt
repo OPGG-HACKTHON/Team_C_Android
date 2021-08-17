27f5d84f63ed898fdafdea1ec45caf278a08e20b
Mino <k3000js@naver.com>
2021-08-18 00:25:00 +0900

    data 추가

---
+++ app/src/main/java/android/milestone/network/request/LoginRequest.kt
@@ -0,0 +1,5 @@
+package android.milestone.network.request
+
+data class LoginRequest(
+    val id : Int
+)
---
+++ app/src/main/java/android/milestone/network/response/SignUpResponse.kt
@@ -0,0 +1,7 @@
+package android.milestone.network.response
+
+data class SignUpResponse(
+    val `data`: String,
+    val status: Int,
+    val success: Boolean
+)
