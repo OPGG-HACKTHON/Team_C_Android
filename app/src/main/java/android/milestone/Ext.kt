c2791220be1c7041f341d49c1082766cc2f0998a
Mino <k3000js@naver.com>
2021-08-18 00:25:42 +0900

    isValidName으로 함수명 변경

--- app/src/main/java/android/milestone/Ext.kt
+++ app/src/main/java/android/milestone/Ext.kt
@@ -2,7 +2,7 @@ package android.milestone
 
 import java.util.regex.Pattern
 
-fun CharSequence.validation(): Boolean {
+fun CharSequence.isValidName(): Boolean {
     val patternString = Pattern.compile("^[a-zA-Z0-9가-힁ㄱ-ㅎ\\s]+$")
     return if (this.length < 2) {
         false
