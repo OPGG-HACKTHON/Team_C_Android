ea0c84c56234a8a436bb306684f1b571eb5be82f
Mino <k3000js@naver.com>
2021-08-29 22:28:28 +0900

    PrefUtil 변수 추가 / App.context 추가

--- app/src/main/java/android/milestone/PrefUtil.kt
+++ app/src/main/java/android/milestone/PrefUtil.kt
@@ -1,33 +1,21 @@
 package android.milestone
 
 import android.content.Context
-import android.content.SharedPreferences
 import androidx.core.content.edit
 
 object PrefUtil {
     private const val DEF_PREF_NAME = "Sweep"
 
+    private val prefUtil =
+        App.instance.applicationContext.getSharedPreferences(DEF_PREF_NAME, Context.MODE_PRIVATE)
 
-    private fun getPref(context: Context, name: String): SharedPreferences {
-        return context.getSharedPreferences(name, Context.MODE_PRIVATE)
-    }
-
-    fun setStringValue(context: Context, name: String, value: String) {
-        val pref = getPref(
-            context,
-            DEF_PREF_NAME
-        )
-        pref.edit {
-            putString(name,value)
+    fun setStringValue(name: String, value: String) {
+        prefUtil.edit {
+            putString(name, value)
             apply()
         }
     }
 
-    fun getStringValue(context: Context, name: String, defValue: String): String? {
-        val pref = getPref(
-            context,
-            DEF_PREF_NAME
-        )
-        return pref.getString(name, defValue)
-    }
+    fun getStringValue(name: String, defValue: String): String? = prefUtil.getString(name, defValue)
+
 }
