0bfcc31881312b97a56970db0e584bcbc37518fc
Mino <k3000js@naver.com>
2021-08-30 21:43:08 +0900

    RootResponse 수정

--- app/src/main/java/android/milestone/network/response/RootResponse.kt
+++ app/src/main/java/android/milestone/network/response/RootResponse.kt
@@ -5,8 +5,11 @@ import com.google.gson.annotations.SerializedName
 data class RootResponse(
     @SerializedName("data")
     val data: String,
+    @SerializedName("msg")
+    val msg : String,
     @SerializedName("status")
     val status: Int,
     @SerializedName("success")
     val success: Boolean
+
 )
