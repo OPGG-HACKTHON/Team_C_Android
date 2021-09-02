2cd73d70aaa598b8ef7f9380f0f6edd89f4cebe9
zion830 <zion830@naver.com>
2021-09-02 23:57:59 +0900

    로그인 건너뛰게 했던거 해제

--- app/src/main/java/android/milestone/ui/login/LoginFragment.kt
+++ app/src/main/java/android/milestone/ui/login/LoginFragment.kt
@@ -81,8 +81,6 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
                 }
             })
         }
-        val intent = Intent(requireContext(), MainActivity::class.java)
-        startActivity(intent)
     }
 
     private fun setToken(key: String, value: String) {
