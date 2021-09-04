bf05a02f2b7e32d92faacb14faffe4b00b859e5d
Mino <k3000js@naver.com>
2021-09-05 01:58:11 +0900

    xml 이름 변경

--- app/src/main/java/android/milestone/ui/teamselect/adapter/TeamSelectAdapter.kt
+++ app/src/main/java/android/milestone/ui/teamselect/adapter/TeamSelectAdapter.kt
@@ -2,9 +2,9 @@ package android.milestone.ui.teamselect.adapter
 
 import android.milestone.R
 import android.milestone.base.BaseRecyclerView
-import android.milestone.databinding.ItemTeamSelectBinding
+import android.milestone.databinding.ItemTeamBinding
 import android.milestone.network.model.auth.TeamInfoModel
 
 class TeamSelectAdapter(
-    private val onClick : (TeamInfoModel) -> Unit
-) : BaseRecyclerView<ItemTeamSelectBinding, TeamInfoModel>(R.layout.item_team_select,onClick)
+    private val onClick: (TeamInfoModel) -> Unit
+) : BaseRecyclerView<ItemTeamBinding, TeamInfoModel>(R.layout.item_team, onClick)
