1783c2532e42d3566fdcac289e94b0765031a74f
zion830 <zion830@naver.com>
2021-09-15 21:29:22 +0900

    team select click 이벤트 동작 안하던거 수정

--- app/src/main/java/android/milestone/ui/teamselect/TeamSelectFragment.kt
+++ app/src/main/java/android/milestone/ui/teamselect/TeamSelectFragment.kt
@@ -1,10 +1,13 @@
 package android.milestone.ui.teamselect
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.milestone.R
 import android.milestone.base.BaseFragment
 import android.milestone.databinding.FragmentTeamSelectBinding
 import android.milestone.ui.login.viewmodel.LoginViewModel
 import android.milestone.ui.teamselect.adapter.TeamSelectAdapter
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,18 +16,19 @@ import dagger.hilt.android.AndroidEntryPoint
 class TeamSelectFragment : BaseFragment<FragmentTeamSelectBinding>(R.layout.fragment_team_select) {
 
     private val viewModel: LoginViewModel by activityViewModels()
-    private val teamSelectAdapter: TeamSelectAdapter by lazy {
-        TeamSelectAdapter {
-            viewModel.setTeamId(it.id)
-            binding.btSelect.isEnabled = true
-        }
-    }
+    private lateinit var teamSelectAdapter: TeamSelectAdapter
 
     override fun initViews() {
+        teamSelectAdapter = TeamSelectAdapter {
+            viewModel.setTeamId(it.id)
+            binding.btSelect.isEnabled = true
+            binding.btSelect.setTextColor(Color.WHITE)
+            binding.btSelect.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.blue500))
+        }
         binding.run {
             rvTeam.adapter = teamSelectAdapter
             ivBack.setOnClickListener {
-                it.findNavController().navigate(R.id.action_team_select_to_login)
+                it.findNavController().navigateUp()
             }
             tvAfterSelect.setOnClickListener {
                 it.findNavController().navigate(R.id.action_team_select_to_nickname)
