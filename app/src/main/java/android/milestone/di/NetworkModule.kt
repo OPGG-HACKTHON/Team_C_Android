383142e35545731a73d4023a3aa6ce467b6147d8
Mino <k3000js@naver.com>
2021-09-16 00:40:23 +0900

    맨 처음 토큰 문제 해결

--- app/src/main/java/android/milestone/di/NetworkModule.kt
+++ app/src/main/java/android/milestone/di/NetworkModule.kt
@@ -14,7 +14,6 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
-import javax.inject.Named
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -22,20 +21,6 @@ import javax.inject.Singleton
 object NetworkModule {
     private const val TIME_OUT_SEC = 5L
     private const val BASE_URL = "http://3.37.194.249"
-
-    @Named("accessToken")
-    @Singleton
-    @Provides
-    fun provideAccessToken() =
-        PrefUtil.getStringValue(ACCESS_TOKEN, "")
-    //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjUzLCJpYXQiOjE2MzAyNDczOTksImV4cCI6MTYzMjgzOTM5OSwiaXNzIjoibWlsZXN0b25lIn0.GvrVt75ienAU5mkhD5u75qrGnPmHXrP_Z9-qLLA4be4"
-
-    @Named("refreshToken")
-    @Singleton
-    @Provides
-    fun provideRefreshToken() =
-        PrefUtil.getStringValue(REFRESH_TOKEN, "")
-    //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MzAyNDczOTksImV4cCI6MTYzMjgzOTM5OSwiaXNzIjoibWlsZXN0b25lIn0.ckDFV86Z2VvVEQUvHHkPs6XMUOiLMTn3twrDPjKPRgQ"
 
     @Provides
     fun provideBaseUrl() = BASE_URL
@@ -51,17 +36,13 @@ object NetworkModule {
     // TODO: 2021-08-11 나중에 로그인 토큰 넣어야 할듯??
     @Singleton
     @Provides
-    fun provideOkHttpClient(
-        @Named("accessToken") accessToken: String,
-        @Named("refreshToken") refreshToken: String,
-        interceptor: HttpLoggingInterceptor
-    ): OkHttpClient =
+    fun provideOkHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient =
         OkHttpClient.Builder()
             .addInterceptor {
                 val request = it.request()
                     .newBuilder()
-                    .addHeader("accesstoken", accessToken)
-                    .addHeader("refreshtoken", refreshToken)
+                    .addHeader("accesstoken", PrefUtil.getStringValue(ACCESS_TOKEN, ""))
+                    .addHeader("refreshtoken", PrefUtil.getStringValue(REFRESH_TOKEN, ""))
                     .build()
                 val response = it.proceed(request)
                 response
